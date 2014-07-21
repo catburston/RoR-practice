9bc466029fadc2301f7c6be68c6ec91fe8aa87da
catburston <cat1788@gmail.com>
2014-07-21 15:55:47 +0200

    first tests for iron_find method

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ActiveRecord::Base
   # because we created the class Locations, this command is not necessary
   #set_table_name 'locations'
+  def self.iron_find(i)
+    Location.where(id:i).first
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "testing iron_find method" do
+    Location.create(name: 'SpecTest', city: 'Nowhere')
+    it "iron_find functions as .find" do
+      expect(Location.iron_find(1)).to eq(Location.find(1))
+    end
+  end
+
 end
